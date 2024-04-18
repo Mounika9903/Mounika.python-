f7600fbffb2d7e3da2c8aec6c284b9bacfc5eb19
Mounika9903 <167081754+Mounika9903@users.noreply.github.com>
2024-04-18 20:40:40 +0530

    Create dictnary.py

---
+++ dictnary.py
@@ -0,0 +1,19 @@
+word=input()
+print(word)
+store=dict()
+print(store)
+store['p']=23
+store['q']=12
+store['w']=22
+store[12]=45
+store["hello"]=34
+print(store)
+allkeys=store.keys()
+print(allkeys)
+for ele in allkeys:
+    print(ele)
+for ch in "abcdef":
+    print(ch)
+for val in[12,23,34,45,67,78]:
+    print(val)
+
