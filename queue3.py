259e5f30efddbd3144dcafd337eb818e84a55594
Mounika9903 <167081754+Mounika9903@users.noreply.github.com>
2024-04-20 12:20:58 +0530

    Create queue3.py

---
+++ queue3.py
@@ -0,0 +1,21 @@
+Q=[43,67]
+print(Q)
+Q.append(10)
+Q.append(30)
+Q.append(40)
+Q.append(50)
+Q.append(108)
+Q.append(109)
+print(Q)
+print(Q)
+deletedElement=Q.pop(0)
+print("Deleted Element is",deletedElement)
+print(Q)
+deletedElement=Q.pop(4)
+print("Deleted Element is",deletedElement)
+print(Q)
+
+
+
+
+
